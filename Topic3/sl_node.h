999f045e218016ce081cb8b1d0711b46bf4001aa
ianlarkin <ilarkin002@student.butte.edu>
2016-03-30 17:35:47 +0000

    sl_node

--- Topic3/sl_node.h
+++ Topic3/sl_node.h
@@ -7,11 +7,18 @@
 #pragma once
 
 #include <cstdlib>
-
+#include <string>
+#include <sstream>
+using namespace std;
+//SLNode Class
 class SLNode {
     public:
+        //Constructor - defulats contents_ to 0 and next_node_ to  NULL
         SLNode(int contents = 0, SLNode* nextNode = NULL);
+        //destructor
         ~SLNode();
+        
+        //sets and gets for our two private variables, contents_ and next_node_
         void set_contents(int);
         int contents() const;
         void set_next_node(SLNode*);
