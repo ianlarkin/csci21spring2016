1d74d5fc01eefaeb46bf590975b80180044a9d06
ianlarkin <ilarkin002@student.butte.edu>
2016-03-30 17:36:13 +0000

    slnode.cpp

--- Topic3/sl_node.cpp
+++ Topic3/sl_node.cpp
@@ -6,26 +6,34 @@
 
 #include "sl_node.h"
 
+//Constructor
 SLNode::SLNode(int contents, SLNode* nextNode) {
     set_contents(contents);
     set_next_node(nextNode);
 }
 
+//Destructor
 SLNode::~SLNode() {}
-        
+
+//Function to set contents_
+//One Parameter - contents
 void SLNode::set_contents(int contents) {
     contents_ = contents;
 }
 
+//Getter function of private variable contents_
 int SLNode::contents() const {
     return contents_;
 }
 
+//Function to set next_node_
+//One Parameter - nextNode
 void SLNode::set_next_node(SLNode* nextNode) {
     next_node_ = nextNode;
     
 }
 
+//Getter function of private variable next_node_
 SLNode* SLNode::next_node() const {
     return next_node_;
 }
